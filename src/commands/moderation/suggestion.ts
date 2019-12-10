a27fb2214d3b7239154b214d2afc4a0d59f350ca
Marc <marc@docorga.com>
2019-12-10 20:00:21 +0100

    Fix suggestion command

--- src/commands/moderation/suggestion.ts
+++ src/commands/moderation/suggestion.ts
@@ -1,7 +1,7 @@
 import { INFO_EMBED_COLOR } from '../../components/Constants';
 import { deleteCommandMessages } from '../../components/Utils';
 import { Command, CommandoClient, CommandMessage } from 'discord.js-commando';
-import { RichEmbed } from 'discord.js';
+import { RichEmbed, TextChannel } from 'discord.js';
 
 
 export default class SuggestionModeratorCommand extends Command {
@@ -9,7 +9,7 @@ export default class SuggestionModeratorCommand extends Command {
         super(client, {
             name: 'suggestion',
             aliases: ['sug'],
-            group: 'moderator',
+            group: 'moderation',
             memberName: 'suggestion',
             description: 'Add Suggestion for Discord Server',
             examples: ['<prefix>suggestion Add new channel for comments'],
@@ -20,26 +20,26 @@ export default class SuggestionModeratorCommand extends Command {
 
     private async  verif(msg: CommandMessage) {
         const channel = msg.guild.channels.find('name', 'suggestion');
-        if(channel == null){
-           return await this.createChan(msg)
+        if (channel == null) {
+            return await this.createChan(msg)
         }
         return channel;
     }
-    private createChan(msg: CommandMessage){
-        var channel =   msg.guild.createChannel('suggestion', {type:'text'})
+    private createChan(msg: CommandMessage) {
+        var channel = msg.guild.createChannel('suggestion', { type: 'text' })
         return channel;
-        
-    }
-    
 
-    
-    public async run(msg: CommandMessage, {description}) {
+    }
+
+
+
+    public async run(msg: CommandMessage, { description }) {
         const channel = await this.verif(msg);
-        var chan = msg.guild.channels.get(channel.id);  
-        chan!.send('fffff')  
+        const chan = msg.guild.channels.get(channel.id) as TextChannel;
+        chan!.send('fffff')
         console.log(channel)
-    
-       
+
+
         //console.log(chan)
         return msg.say(channel.id);
     }
