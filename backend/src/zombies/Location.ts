4c535ddae3ff4c75f8174585584ada5c42aff1cf
Marc <marc@docorga.com>
2019-12-12 23:21:50 +0100

    (Zombies) Add first steps for zombies generator

--- backend/src/zombies/Location.ts
+++ backend/src/zombies/Location.ts
@@ -2,4 +2,12 @@ export class ZombiesLocation {
     constructor(public x: number, public y: number) {
 
     }
+
+    distanceTo(location: ZombiesLocation): number {
+        return Math.abs((this.x - location.x) + (this.y - location.y));
+    }
+
+    isInBounds(mapSize: number): boolean {
+        return this.x >= 0 && this.y >= 0 && (this.x < mapSize) && (this.y < mapSize)
+    }
 }
--- backend/src/zombies/Map.ts
+++ backend/src/zombies/Map.ts
@@ -2,21 +2,31 @@ import { ZombiesTile } from "./Tile";
 import { ZombiesLocation } from "./Location";
 import { Type } from "class-transformer";
 
+export const ZombiesMapSettings = {
+    easy: {
+        zombiesMultiplicator: 1,
+        safeRange: 4
+    }
+}
+
+function randomBetween(min: number, max: number, decimalPlaces: number = 0) {
+    const rand = Math.random() * (max - min);
+    const power = Math.pow(10, decimalPlaces);
+    return Math.floor(rand * power) / power;
+}
+
 export class ZombiesMap {
     @Type(() => ZombiesTile)
-    private tiles: ZombiesTile[][];
+    public tiles: ZombiesTile[][];
     @Type(() => ZombiesLocation)
-    private cityLocation: ZombiesLocation;
+    public cityLocation: ZombiesLocation;
+    public zombiesCount: number = 0;
 
-    constructor(private size: number) { }
-
-    randomClampToSize() {
-        return Math.floor(Math.random() * (this.size));
-    }
+    constructor(public size: number, public mode: string = 'easy') { }
 
     generate() {
         this.tiles = [];
-        this.cityLocation = new ZombiesLocation(this.randomClampToSize(), this.randomClampToSize());
+        this.cityLocation = new ZombiesLocation(randomBetween(0, this.size), randomBetween(0, this.size));
         for (let x = 0; x < this.size; x++) {
             this.tiles[x] = [];
             for (let y = 0; y < this.size; y++) {
@@ -25,8 +35,63 @@ export class ZombiesMap {
         }
     }
 
-    isCity(x, y) {
+    populate(day: number) {
+        const zMultiplicator = ZombiesMapSettings[this.mode].zombiesMultiplicator;
+
+        /**
+         * stable part of horde
+         * (day 1, easy, 10 tiles) = 10z
+         * 
+         */
+        this.zombiesCount = this.size * zMultiplicator * day;
+
+        /**
+         * random part of horde
+         */
+        this.zombiesCount += randomBetween(0, 6) * day * zMultiplicator;
+
+        for (let z = 0; z < this.zombiesCount; z++) {
+
+        }
+    }
+
+    private isCity(x, y) {
         return this.cityLocation.x === x && this.cityLocation.y === y;
+    }
+
+    /**
+     * Get an area arround a location (clamped to map)
+     * @param location 
+     * @param size 
+     */
+    getArea(location: ZombiesLocation, size: number): ZombiesTile[] {
+        let result: ZombiesTile[] = [];
+
+        if (size < 3 || size % 2 === 0) {
+            throw new Error('Area size should be odd number')
+        }
+
+        for (let i = location.x - (Math.floor(size / 2)); i <= location.x + (Math.floor(size / 2)); i++) {
+            for (let j = location.y - (Math.floor(size / 2)); j <= location.y + (Math.floor(size / 2)); j++) {
+                let locationToTest = new ZombiesLocation(i, j);
+
+                if (!locationToTest.isInBounds(this.size)) {
+                    continue;
+                }
+
+                result.push(this.tiles[i][j])
+            }
+        }
+
+        return result;
+    }
+
+    getTileByLocation(location: ZombiesLocation): ZombiesTile | null {
+        if (!location.isInBounds(this.size)) {
+            return null;
+        }
+
+        return this.tiles[location.x][location.y]
     }
 
     toString() {
