9fb4c041234fda5d4154f126bb2fb401a4572767
Marc <marc@docorga.com>
2021-04-15 19:31:25 +0200

    [backend] Remove cah

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,9 @@ import "reflect-metadata";
 import { client } from './bot';
 import { Server } from './api/server';
 
-import { init as CAH } from './cah';
-
 // Start discord bot
 client.login(process.env.DISCORD_TOKEN)
 
 client.on('providerReady', () => {
-    CAH(client);
     new Server(process.env.RED_API_PORT, client)
 });
